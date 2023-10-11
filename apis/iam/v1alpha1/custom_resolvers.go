115347c2efad8c26744e34b92eeeed3c8feebaae
Christopher Haar <christopher.haar@upbound.io>
2023-10-12 00:18:58 +0200

    feat(bump): update runtime
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- apis/iam/v1alpha1/custom_resolvers.go
+++ apis/iam/v1alpha1/custom_resolvers.go
@@ -16,40 +16,40 @@ limitations under the License.
 
 package v1alpha1
 
-// import (
-// 	"context"
+import (
+	"context"
 
-// 	"github.com/crossplane/crossplane-runtime/pkg/errors"
-// 	"github.com/crossplane/crossplane-runtime/pkg/reference"
-// 	"sigs.k8s.io/controller-runtime/pkg/client"
-// )
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/reference"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
-// func (mg *Token) ResolveReferences(ctx context.Context, c client.Reader) error {
-// 	r := reference.NewAPIResolver(c, mg)
+func (mg *Token) ResolveReferences(ctx context.Context, c client.Reader) error {
+	r := reference.NewAPIResolver(c, mg)
 
-// 	var ref reference.To
-// 	switch mg.Spec.ForProvider.Owner.Type {
-// 	case "robots":
-// 		ref = reference.To{
-// 			List:    &RobotList{},
-// 			Managed: &Robot{},
-// 		}
-// 	default:
-// 		return errors.New("only robots is supported as owner type for reference resolution")
-// 	}
+	var ref reference.To
+	switch mg.Spec.ForProvider.Owner.Type {
+	case "robots":
+		ref = reference.To{
+			List:    &RobotList{},
+			Managed: &Robot{},
+		}
+	default:
+		return errors.New("only robots is supported as owner type for reference resolution")
+	}
 
-// 	rsp, err := r.Resolve(ctx, reference.ResolutionRequest{
-// 		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.Owner.ID),
-// 		Extract:      reference.ExternalName(),
-// 		Reference:    mg.Spec.ForProvider.Owner.IDRef,
-// 		Selector:     mg.Spec.ForProvider.Owner.IDSelector,
-// 		To:           ref,
-// 	})
-// 	if err != nil {
-// 		return errors.Wrap(err, "mg.Spec.ForProvider.Owner.ID")
-// 	}
-// 	mg.Spec.ForProvider.Owner.ID = reference.ToPtrValue(rsp.ResolvedValue)
-// 	mg.Spec.ForProvider.Owner.IDRef = rsp.ResolvedReference
+	rsp, err := r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.Owner.ID),
+		Extract:      reference.ExternalName(),
+		Reference:    mg.Spec.ForProvider.Owner.IDRef,
+		Selector:     mg.Spec.ForProvider.Owner.IDSelector,
+		To:           ref,
+	})
+	if err != nil {
+		return errors.Wrap(err, "mg.Spec.ForProvider.Owner.ID")
+	}
+	mg.Spec.ForProvider.Owner.ID = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.Owner.IDRef = rsp.ResolvedReference
 
-// 	return nil
-// }
+	return nil
+}
